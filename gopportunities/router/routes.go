d881e308d249c693764a168377eeaa8ada9552b4
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-28 20:27:50 -0300

    feat: update handler structure to separate subpackage

--- gopportunities/router/routes.go
+++ gopportunities/router/routes.go
@@ -2,31 +2,15 @@ package router
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/render"
-	"net/http"
+	"github.com/valdenidelgado/go-projects/gopportunities/handler"
 )
 
 func initializeRoutes(router *chi.Mux) {
 	router.Route("/api/v1", func(r chi.Router) {
-		r.Get("/opening", func(w http.ResponseWriter, r *http.Request) {
-			render.Status(r, http.StatusOK)
-			render.JSON(w, r, map[string]string{"msg": "hello"})
-		})
-		r.Post("/opening", func(w http.ResponseWriter, r *http.Request) {
-			render.Status(r, http.StatusOK)
-			render.JSON(w, r, map[string]string{"msg": "hello"})
-		})
-		r.Delete("/opening", func(w http.ResponseWriter, r *http.Request) {
-			render.Status(r, http.StatusOK)
-			render.JSON(w, r, map[string]string{"msg": "hello"})
-		})
-		r.Put("/opening", func(w http.ResponseWriter, r *http.Request) {
-			render.Status(r, http.StatusOK)
-			render.JSON(w, r, map[string]string{"msg": "hello"})
-		})
-		r.Get("/openings", func(w http.ResponseWriter, r *http.Request) {
-			render.Status(r, http.StatusOK)
-			render.JSON(w, r, map[string]string{"msg": "hello"})
-		})
+		r.Get("/opening", handler.ShowOpeningHandler)
+		r.Post("/opening", handler.CreateOpeningHandler)
+		r.Delete("/opening", handler.DeleteOpeningHandler)
+		r.Put("/opening", handler.UpdateOpeningHandler)
+		r.Get("/openings", handler.ListOpeningHandler)
 	})
 }
