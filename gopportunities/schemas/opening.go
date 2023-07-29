c454d4ea4d72850c7c17690a018414a0c50254de
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-29 13:00:29 -0300

    feat add opening response json

--- gopportunities/schemas/opening.go
+++ gopportunities/schemas/opening.go
@@ -1,6 +1,9 @@
 package schemas
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Opening struct {
 	gorm.Model
@@ -10,4 +13,17 @@ type Opening struct {
 	Link     string
 	Remote   bool
 	Salary   int64
+}
+
+type OpeningResponse struct {
+	ID        uint      `json:"id"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Link      string    `json:"link"`
+	Remote    bool      `json:"remote"`
+	Salary    int64     `json:"salary"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deletedAt,omitempty"`
 }
