3c468c7c3f7dccf6c708e231a0b1c9fb63e99cde
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-31 13:56:34 -0300

    feat add update opening

--- gopportunities/handler/request.go
+++ gopportunities/handler/request.go
@@ -39,3 +39,20 @@ func (r *CreateOpeningRequest) Validate() error {
 func errParamIsRequired(name, typ string) error {
 	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
 }
+
+type UpdateOpeningRequest struct {
+	ID       string `json:"id"`
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Link != "" || r.Remote != nil || r.Salary > 0 {
+		return nil
+	}
+	return fmt.Errorf("request body is empty or malformed")
+}
--- gopportunities/handler/updateOpening.go
+++ gopportunities/handler/updateOpening.go
@@ -1,12 +1,60 @@
 package handler
 
 import (
-	"github.com/go-chi/render"
+	"encoding/json"
+	"github.com/valdenidelgado/go-projects/gopportunities/schemas"
 	"net/http"
 )
 
 func UpdateOpeningHandler(w http.ResponseWriter, r *http.Request) {
-	render.Status(r, http.StatusOK)
-	render.JSON(w, r, map[string]string{"msg": "hello"})
+	request := UpdateOpeningRequest{}
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		sendError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Error validating request: %v", err.Error())
+		sendError(w, http.StatusBadRequest, err.Error())
+		return
+	}
 
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		sendError(w, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, request.ID).Error; err != nil {
+		sendError(w, http.StatusNotFound, err.Error())
+		return
+	}
+
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
+	if request.Company != "" {
+		opening.Company = request.Company
+	}
+	if request.Location != "" {
+		opening.Location = request.Location
+	}
+	if request.Link != "" {
+		opening.Link = request.Link
+	}
+	if request.Remote != nil {
+		opening.Remote = *request.Remote
+	}
+	if request.Salary > 0 {
+		opening.Salary = request.Salary
+	}
+
+	if err := db.Save(&opening).Error; err != nil {
+		logger.Errorf("error updating request: %v", err)
+		sendError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	sendSuccess(w, opening)
 }
