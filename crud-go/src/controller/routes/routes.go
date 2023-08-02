48c138c11571702045a27afe411af1f13cc5bc7d
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-08-02 09:45:55 -0300

    create controllers handlers and add function on routes files

--- crud-go/src/controller/routes/routes.go
+++ crud-go/src/controller/routes/routes.go
@@ -1,11 +1,14 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/valdenidelgado/go-projects/crud-go/src/controller"
+)
 
 func InitRoutes(r *gin.RouterGroup) {
-	r.GET("/getUserById/:userId")
-	r.GET("/getUserEmail/:userEmail")
-	r.POST("/createUser")
-	r.PUT("/updateUser/:userId")
-	r.DELETE("/deleteUser/:userId")
+	r.GET("/getUserById/:userId", controller.FindUserById)
+	r.GET("/getUserEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/createUser", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
 }
