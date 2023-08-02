aaef1d9c77e4411da125f4b7031dc2bf0d0cd7f9
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-08-02 10:05:09 -0300

    init router and server on port :8080

--- crud-go/main.go
+++ crud-go/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/valdenidelgado/go-projects/crud-go/src/controller/routes"
 	"log"
-	"os"
 )
 
 func main() {
@@ -12,5 +12,11 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	fmt.Println(os.Getenv("TEST"))
+
+	router := gin.Default()
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err.Error())
+	}
 }
