de4f2cad68ddf97eeebbeb7b45f5801e630e7b76
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-08-02 10:38:59 -0300

    add validation to user request

--- crud-go/src/controller/model/request/user_request.go
+++ crud-go/src/controller/model/request/user_request.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Email    string `json:"email"`
-	Name     string `json:"name"`
-	Age      int8   `json:"age"`
-	Password string `json:"password"`
+	Email    string `json:"email" binding:"required,email"`
+	Name     string `json:"name" binding:"required,min=4,max=50"`
+	Age      int8   `json:"age" binding:"required,min=1,max=120"`
+	Password string `json:"password" binding:"required,min=6,max=50,containsany=!@#$%^&*()_+"`
 }
