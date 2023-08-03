0ae11e14d0ef66a988a3f3ad724001b1ee884ce9
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-08-03 10:30:55 -0300

    refactor: add loggers on createUser file

--- crud-go/src/controller/createUser.go
+++ crud-go/src/controller/createUser.go
@@ -2,20 +2,29 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/valdenidelgado/go-projects/crud-go/src/configuration/logger"
 	"github.com/valdenidelgado/go-projects/crud-go/src/configuration/validation"
 	"github.com/valdenidelgado/go-projects/crud-go/src/controller/model/request"
-	"log"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Creating a new user",
+		zap.String("journey", "createUser"),
+	)
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		log.Printf("Error trying to bind JSON: %s", err.Error())
+		logger.Error("Error while trying to bind user request", err,
+			zap.String("journey", "createUser"),
+		)
 		errRest := validation.ValidateUserError(err)
 		c.JSON(errRest.Code, errRest)
 		return
 	}
 
+	logger.Info("User created successfully",
+		zap.String("journey", "createUser"),
+	)
 	c.JSON(200, gin.H{"message": "User created successfully"})
 }
