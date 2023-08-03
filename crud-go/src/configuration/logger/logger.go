2d6e8ba7755d022811684f9795074c4938313e48
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-08-03 10:24:28 -0300

    feat: add logger config

---
+++ crud-go/src/configuration/logger/logger.go
@@ -0,0 +1,69 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+	"strings"
+)
+
+var (
+	log        *zap.Logger
+	LOG_OUTPUT = "LOG_OUTPUT"
+	LOG_LEVEL  = "LOG_LEVEL"
+)
+
+func init() {
+	logConfig := zap.Config{
+		OutputPaths: []string{getOutputLogs()},
+		Level:       zap.NewAtomicLevelAt(getLevelLogs()),
+		Encoding:    "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:     "level",
+			TimeKey:      "time",
+			MessageKey:   "message",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+
+	log, _ = logConfig.Build()
+}
+
+func Info(message string, tags ...zap.Field) {
+	log.Info(message, tags...)
+	if err := log.Sync(); err != nil {
+		panic(err)
+	}
+}
+
+func Error(message string, err error, tags ...zap.Field) {
+	tags = append(tags, zap.NamedError("error", err))
+	log.Info(message, tags...)
+	if err := log.Sync(); err != nil {
+		panic(err)
+	}
+}
+
+func getOutputLogs() string {
+	output := strings.ToLower(strings.TrimSpace(os.Getenv(LOG_OUTPUT)))
+	if output == "" {
+		return "stdout"
+	}
+	return output
+}
+
+func getLevelLogs() zapcore.Level {
+	level := strings.ToLower(strings.TrimSpace(os.Getenv(LOG_LEVEL)))
+	switch level {
+	case "debug":
+		return zap.DebugLevel
+	case "info":
+		return zap.InfoLevel
+	case "error":
+		return zap.ErrorLevel
+	default:
+		return zap.InfoLevel
+	}
+}
