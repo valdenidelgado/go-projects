2cff8e5c0a2e191b7c35836812654dfd1b3b02fb
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-08-10 13:30:22 -0300

    feat: add delete user controller

--- crud-go/src/controller/delete_user.go
+++ crud-go/src/controller/delete_user.go
@@ -1,7 +1,41 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/valdenidelgado/go-projects/crud-go/src/configuration/logger"
+	"github.com/valdenidelgado/go-projects/crud-go/src/configuration/rest_err"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.uber.org/zap"
+	"net/http"
+)
 
 func (uc *userControllerInterface) DeleteUser(c *gin.Context) {
+	logger.Info("Deleting a new user",
+		zap.String("journey", "deleteUser"),
+	)
 
+	userId := c.Param("id")
+	if _, err := primitive.ObjectIDFromHex(userId); err != nil {
+		logger.Error("Error while trying to convert userId to Hex", err,
+			zap.String("journey", "deleteUser"),
+		)
+		errRest := rest_err.NewBadRequestError("Invalid id, must be a valid hex")
+		c.JSON(errRest.Code, errRest)
+	}
+
+	err := uc.service.DeleteUser(userId)
+	if err != nil {
+		logger.Error("Error trying to call DeleteUser service", err,
+			zap.String("journey", "deleteUser"),
+		)
+		c.JSON(err.Code, err)
+		return
+	}
+
+	logger.Info("User deleted successfully",
+		zap.String("id", userId),
+		zap.String("journey", "deleteUser"),
+	)
+
+	c.Status(http.StatusOK)
 }
